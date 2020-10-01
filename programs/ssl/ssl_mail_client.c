dfa609d892c05945cc743fe4612258e45d3bf39a
Ubuntu <ubuntu@ip-172-31-5-97.us-west-2.compute.internal>
2020-10-01 05:59:42 +0000

    Removed RSA as a condition since tls 1.3 doesn't support RSA anymore

--- programs/ssl/ssl_mail_client.c
+++ programs/ssl/ssl_mail_client.c
@@ -43,13 +43,15 @@
 #define MBEDTLS_EXIT_FAILURE    EXIT_FAILURE
 #endif /* MBEDTLS_PLATFORM_C */
 
+
+// RSA not defined on purpose: MBEDTLS_RSA_C
 #if !defined(MBEDTLS_BIGNUM_C) || !defined(MBEDTLS_ENTROPY_C) ||  \
     !defined(MBEDTLS_SSL_TLS_C) || !defined(MBEDTLS_SSL_CLI_C) || \
-    !defined(MBEDTLS_NET_C) || !defined(MBEDTLS_RSA_C) ||         \
+    !defined(MBEDTLS_NET_C) ||         \
     !defined(MBEDTLS_CTR_DRBG_C) || !defined(MBEDTLS_X509_CRT_PARSE_C) || \
     !defined(MBEDTLS_FS_IO)
 int main( void )
-{
+{	
     mbedtls_printf("MBEDTLS_BIGNUM_C and/or MBEDTLS_ENTROPY_C and/or "
            "MBEDTLS_SSL_TLS_C and/or MBEDTLS_SSL_CLI_C and/or "
            "MBEDTLS_NET_C and/or MBEDTLS_RSA_C and/or "
